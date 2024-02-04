35ca5c1f8b3ed3dbd973ce5d362da84fb35ba300
DixonKH <khaydarovdilshod9@gmail.com>
2024-02-04 22:13:25 +0900

    yangi ozgarish

--- server.js
+++ server.js
@@ -19,28 +19,27 @@ app.set("views", "views");                   // bu yerda biz views degan folder 
 app.set("view engine", "ejs");                 // hamda view folder engine ni ejs ekanligini korsatyabmiz bu views folderini oqiydi
 
 
-//4.  Routing code: bu routerlarga moljallangan bolim
+//4.  Routing code: bu routerlarga moljallangan bolim 
 
-app.get("/hello", function(req, res) {
-    res.end(`<h1 style="color: red">Hello World</h1>`);
-});
-
-app.get("/gift", function(req, res) {  
-    res.end(`<h1 style="color: blue">Siz sovgalar bolimidasiz</h1>`);
-}); 
-
-// app.post("/create-item", (req, res) => {
-//     console.log(req.body);
-//     res.json({test: "success"});
-// })
-
-// app.get("/", function (req, res) {
-//     res.render("harid");
+// app.get("/hello", function(req, res) {
+//     res.end(`<h1 style="color: red">Hello World</h1>`);
 // });
+
+// app.get("/gift", function(req, res) {  
+//     res.end(`<h1 style="color: blue">Siz sovgalar bolimidasiz</h1>`);
+// }); 
+
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"});
+})
+
+app.get("/", function (req, res) {
+    res.render("harid");
+});
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function() {
     console.log(`Server is running successfully on port: ${PORT}`);
 });
-
