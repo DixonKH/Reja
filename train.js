a2924dbeaf56249828ef756d91c9e23bb3df4f95
DixonKH <khaydarovdilshod9@gmail.com>
2024-02-17 01:48:45 +0900

    feat: Task - D qoshildi

--- train.js
+++ train.js
@@ -1,51 +1,79 @@
 
+//  ===================================  Task - D
+  function checkContent(a, b) {
+        let arr = [];
+        let arr2 = [];
+        if(typeof a == "string" && typeof b == "string") {
+              for(let i = 0; i < a.length; i++) {
+                arr.push(a[i]);
+              };
+              for(let k = 0; k < b.length; k++) {
+                arr2.push(b[k]);
+              }; 
+              const new_arr = arr.sort();
+              const new_arr2 = arr2.sort();
+              const str1 = new_arr.toString();
+              const str2 = new_arr2.toString();
+              if(str1 === str2) {
+                return true;
+              } else {
+                return false;
+              }
+        }
+        
+  }
+
+  const result = checkContent("mitgroup", "gmtiprou");
+
+  console.log(result);
+
 
 
 
 // ====================================  TASK-C
 
 
-class Shop {
-        constructor(non, lagmon, cola) {
-                this.non = non;
-                this.lagmon = lagmon;
-                this.cola = cola;
-        }
+// class Shop {
+//         constructor(non, lagmon, cola) {
+//                 this.non = non;
+//                 this.lagmon = lagmon;
+//                 this.cola = cola;
+//         }
 
-     qoldiq() {
-        const hour = new Date().getHours();
-        const min = new Date().getMinutes();
-        console.log(`Hozir soat ${hour}:${min}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud`);
-     }
+//      qoldiq() {
+//         const hour = new Date().getHours();
+//         const min = new Date().getMinutes();
+//         console.log(`Hozir soat ${hour}:${min}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud`);
+//      }
 
-     sotish(a, maxsulot) {
-                if( typeof a === "string" && a.toLowerCase() === "non") {
-                    this.non -= maxsulot;   
-                } else if( typeof a === "string" && a.toLowerCase() === "lagmon") {
-                        this.lagmon -= maxsulot;
-                }else if(typeof a === "string" && a.toLowerCase() === "cola"){
-                        this.cola -= maxsulot;
-                }
-         } 
+//      sotish(a, maxsulot) {
+//                 if( typeof a === "string" && a.toLowerCase() === "non") {
+//                     this.non -= maxsulot;   
+//                 } else if( typeof a === "string" && a.toLowerCase() === "lagmon") {
+//                         this.lagmon -= maxsulot;
+//                 }else if(typeof a === "string" && a.toLowerCase() === "cola"){
+//                         this.cola -= maxsulot;
+//                 }
+//          } 
          
-     qabul(b, maxsulot) {
-                if( typeof b === "string" && b.toLowerCase() === "non") {
-                    this.non += maxsulot;   
-                } else if( typeof b === "string" && b.toLowerCase() === "lagmon") {
-                        this.lagmon += maxsulot;
-                }else if(typeof b === "string" && b.toLowerCase() === "cola"){
-                        this.cola += maxsulot;
-                }
-         } 
-}
+//      qabul(b, maxsulot) {
+//                 if( typeof b === "string" && b.toLowerCase() === "non") {
+//                     this.non += maxsulot;   
+//                 } else if( typeof b === "string" && b.toLowerCase() === "lagmon") {
+//                         this.lagmon += maxsulot;
+//                 }else if(typeof b === "string" && b.toLowerCase() === "cola"){
+//                         this.cola += maxsulot;
+//                 }
+//          } 
+// }
 
-const shop = new Shop(4, 5, 2);
+// const shop = new Shop(4, 5, 2);
 
 
- shop.qoldiq();
- shop.sotish("Non", 2);
- shop.qabul("cola", 5);
- shop.qoldiq();
+//  shop.qoldiq();
+//  shop.sotish("Non", 2);
+//  shop.qabul("cola", 5);
+//  shop.qoldiq();
 
 
 //   ===============================  Task B
